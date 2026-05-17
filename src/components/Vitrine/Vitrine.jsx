df780e486b5235b41c2460f5fe59c9056486e611
Carlos-Santos-0 <carlos.silva3@aluno.ifsp.edu.br>
2026-05-17 14:49:23 -0300

    Atualizando tela cardapio/responsividade da tela

--- src/components/Vitrine/Vitrine.jsx
+++ src/components/Vitrine/Vitrine.jsx
@@ -15,36 +15,24 @@ function Vitrine () {
                 <h1 className={styles.titulo}>Nosso Cardápio</h1>
                 <h3 className={styles.subTitulo}>Escolha suas marmitas favoritas</h3>
                 </div>
-                {/*<div className={styles.vitrine_marmitas}> 
-                        <div className={styles.card_pacote}> 
-                            <h3>Pacote Semanal</h3>
-                            <p>Marmitas customizadas para semana toda, peça até sexta e receba frescas todos os dias adaptadas e especializadas para sua dieta</p>
-                            <img className={styles.imagemPacote} src="" alt="imgem" />
-                            <Link className={styles.botao_pedir}>Pedir</Link>
-                        </div>
-                        <div className={styles.card_pacote}> 
-                            <h3>Pacote Mensal</h3>
-                            <p>Marmitas customizadas para semana toda, peça até sexta e receba frescas todos os dias adaptadas e especializadas para sua dieta</p>
-                            <img className={styles.imagemPacote} src="" alt="imgem" />
-                            <Link className={styles.botao_pedir}>Pedir</Link>
-                        </div>
-                </div>
-                <hr className={styles.separador}/>*/}
-                {/* produtos */}
 
                 <div className={styles.vitrine_produtos}>
                     
                     {vitrineDados.map((produto) => (
                         <div key={produto.id} className={styles.card_produto}>
 
-                        
+                        <div className={styles.CardImagem}>
                         <img src={produto.imagem} alt={produto.nome} className={styles.imagemProduto} />
-                        <h3 className={styles.nomeProduto}>{produto.nome}</h3>
-                        <p className={styles.descricaoProduto}>{produto.descricao}</p>
+                        </div>
+                        <div className={styles.infoProduto}>
 
-                        <div className={styles.rodapeProduto}>
-                            <span className={styles.preco}>R$ {produto.preco}</span>
-                            <button className={styles.botaoComprar}>Adicionar ao Carrinho</button>
+                            <h3 className={styles.nomeProduto}>{produto.nome}</h3>
+                            <p className={styles.descricaoProduto}>{produto.descricao}</p>
+
+                            <div className={styles.rodapeProduto}>
+                                <span className={styles.preco}>R$ {produto.preco}</span>
+                                <button className={styles.botaoComprar}>Adicionar ao Carrinho</button>
+                            </div>
                         </div>
 
                     </div>
