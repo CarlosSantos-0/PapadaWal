90c943fe45557f5696f420f1d1e2d9227aef0fc5
hdarlim <henriquedarlim@gmail.com>
2026-06-01 19:50:47 -0300

    Changed "Papa da da Wal" to "Papa da Wal"

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -19,7 +19,7 @@ function Sobre() {
           <div className={styles.texto}>
             <h1>Sobre Nós</h1>
             <p>
-              A Papa da da Wal nasceu da vontade de oferecer uma gastronomia saudável e acessível para todas as idades. A ideia surgiu ao perceber a dificuldade em encontrar alimentos realmente saudáveis no mercado, já que muitos produtos são altamente industrializados. Com mais de 22 anos de experiência na gastronomia, a fundadora criou a marca com o propósito de levar refeições práticas, saborosas e nutritivas, atendendo desde crianças em fase de introdução alimentar até pessoas idosas. Há cerca de seis meses no mercado, a Papa da Wal acredita que comer bem é uma forma de cuidado e carinho, oferecendo uma alimentação 100% saudável e preparada com dedicação em cada detalhe.
+              A Papa da Wal nasceu da vontade de oferecer uma gastronomia saudável e acessível para todas as idades. A ideia surgiu ao perceber a dificuldade em encontrar alimentos realmente saudáveis no mercado, já que muitos produtos são altamente industrializados. Com mais de 22 anos de experiência na gastronomia, a fundadora criou a marca com o propósito de levar refeições práticas, saborosas e nutritivas, atendendo desde crianças em fase de introdução alimentar até pessoas idosas. Há cerca de seis meses no mercado, a Papa da Wal acredita que comer bem é uma forma de cuidado e carinho, oferecendo uma alimentação 100% saudável e preparada com dedicação em cada detalhe.
             </p>
           </div>
 
